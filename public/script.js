431f88e547ee282a598a7642da307fb02b914180
Shreeya <shreeya.s@siemens.com>
2022-08-29 18:58:59 +0530

    updated

--- public/script.js
+++ public/script.js
@@ -1,50 +1,28 @@
-get_lat_long().then(data =>{
-  latitude = data[0].lat;
-  longitude = data[0].lon;
-  fetch('/weather', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    },
-    body: JSON.stringify({
-      "latitude": latitude,
-      "longitude": longitude
+const icon = new Skycons({ color: "blue" });
+icon.set("icon", "partly-cloudy-day");
+icon.play();
 
+function update() {
+    place = document.getElementById("data-city-search").value;
+    fetch("/weather", {
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify({ location: place }),
     })
-  }).then(res => res.json()).then(data => {
-    setWeatherData(data);
-  })
-
-})
-
-const icon = new Skycons({ color: 'blue' })
-const locationElement = document.querySelector('[data-location]');
-const statusElement = document.querySelector('[data-status]')
-const temperatureElement = document.querySelector('[data-temperature]')
-const precipitationElement = document.querySelector('[data-humidity]')
-const windElement = document.querySelector('[data-wind]')
-
-icon.set('icon', 'partly-cloudy-day')
-icon.play()
-
-function setWeatherData(data){
-  console.log(data);
-  locationElement.textContent = data.name;
-  statusElement.textContent = data.weather[0].description;
-  console.log(data.weather[0].description);
-  temperatureElement.textContent = data.main.temp;
-  windElement.textContent = data.wind.speed;
-  precipitationElement.textContent = data.main.humidity;
-
-}
-
-
-async function get_lat_long(){
-  let place = await document.getElementById('data-city-search').value;
-  console.log(place);
-  let response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${place}&limit=5&appid=c2a4f243c670c89b4bfe6329f90f958d`)
-  let data = await response.json();
-  console.log(data)
-  return data;
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            wind = data.wind.speed;
+            temperature = data.main.temp;
+            humidity = data.main.humidity;
+            console.log(data);
+            document.getElementById('data-wind').innerHTML = wind;
+            document.getElementById('data-temperature').innerHTML = temperature;
+            document.getElementById('data-humidity').innerHTML = humidity;
+            
+        });
 }
--- server.js
+++ server.js
@@ -1,25 +1,43 @@
-require('dotenv').config();
-const OPEN_WEATHER_API = process.env.OPEN_WEATHER_API;
-const express = require('express');
+require("dotenv").config();
+const fetch = require("node-fetch");
+const express = require("express");
+const bodyParser = require("body-parser");
+const { response } = require("express");
+
 const app = express();
-const axios = require('axios');
+
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
 
+const OPEN_WEATHER_API = process.env.OPEN_WEATHER_API;
 
-app.post('/weather',(req,res)=>{
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${req.body.latitude}&lon=${req.body.longitude}&appid=${OPEN_WEATHER_API}`;
-    axios({
-        url: url,
-        responseType: 'json'
-      }).then( data =>{
-        res.json(data.data);
-      })
-
-
-})
-
-app.listen(3000,()=>{
-    console.log('Listening')
+app.post("/weather", (req, res) => {
+    place = req.body.location;
+    console.log(place);
+    url = `http://api.openweathermap.org/geo/1.0/direct?q=${place}&limit=5&appid=${OPEN_WEATHER_API}`;
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            latitude = data[0].lat;
+            longitude = data[0].lon;
+            url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${OPEN_WEATHER_API}`;
+            fetch(url)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    res.json(data);
+                });
+        });
 });
 
+app.listen(3000, () => {
+    console.log("Listening");
+});
